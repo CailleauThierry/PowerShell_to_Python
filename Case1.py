1f7818dbc9a818ce076fae737311272a351d56a9
CailleauThierry <thierrycailleau@yahoo.fr>
2018-12-09 17:12:27 +0100

    Initial commit

---
+++ Case1.py
@@ -0,0 +1,13 @@
+Name = 'Thierry'
+name = 'Cailleau'
+print(Name)
+print(name)
+Print(name)
+# returns in vscode cmd.exe
+# (base) C:\Users\tcailleau\Documents\Python\PowerShell_to_Python>C:/ProgramData/Anaconda3/python3.exe c:/Users/tcailleau/Documents/Python/PowerShell_to_Python/Case1.py
+# Thierry
+# Cailleau
+# Traceback (most recent call last):
+#   File "c:/Users/tcailleau/Documents/Python/PowerShell_to_Python/Case1.py", line 5, in <module>
+#     Print(name)
+# NameError: name 'Print' is not defined
